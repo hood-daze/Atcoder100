b0d54ff41255e809b707e71fd840bd2a83533497
sotakoyama <koyama3@ryomo.co.jp>
2023-03-02 06:48:12 +0900

    12問目に取り掛かった。

--- 12/12.cpp
+++ 12/12.cpp
@@ -7,31 +7,27 @@ using ll = long long;
 int main()
 {
     // 数の整理
-    // N = XXの数
-
-    // 条件の整理
-    // 
+    // N = 国会議員の数
+    // M = 人間関係の数(x,y)
     
+    // 条件の整理
+    // 2のn乗り物個の組み合わせがある。
+
+
     // 回答
     int N, M; cin >> N >> M;
+    vector<pair<int, int>> xy[10];
     for (int i = 0; i < M; i++)
     {
-        int k; cin >> k;
-        for (int i = 0; i < k; i++)
-        {
-
-        }
-
+        int x, y; cin >> x >> y;
+        xy[i].push_back(make_pair(x, y));
+        // setで重複をなくすとか？
     }
+    // わからない・・・
 
     cout << "";
 }
 
-
-// コピペで使える便利変数宣言
-
-
-// int N; cin >> N;
 
 
 // 配列の宣言
@@ -47,4 +43,8 @@ int main()
 //
 //}
 
+
+
+
+// 答え。
 
