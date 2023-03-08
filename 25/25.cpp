6e458991fcd0677f54a167278b85d9ae786d2b11
sotakoyama <koyama3@ryomo.co.jp>
2023-03-08 22:24:09 +0900

    25をといた。

---
+++ 25/25.cpp
@@ -0,0 +1,107 @@
+﻿#include <stdio.h>
+#include <iostream>
+#include <algorithm>
+#include <math.h>
+#include <stack>
+
+using namespace std;
+
+
+// XXの問題
+// 整理
+// w,h = 地図の幅と高さ
+// ci, j  i, jは1からはじまる。0= 海, 1= 陸
+// 0 0 入力の終わり
+
+
+
+int main() {
+
+	while (true) {
+		int w, h;
+		scanf("%d", &w);// 達人はscanfを使用している? intだからかな？
+		scanf("%d", &h);
+		if (w == 0 && h == 0) break;
+
+		int table[101][101];
+
+		for (int i = 1; i <= h; i++) {
+			for (int k = 1; k <= w; k++) {
+				cin >> table[h][w];				 
+			}
+		}
+	}
+
+	return 0;
+}
+
+
+#include <stdio.h>
+#include <cmath>
+#include <algorithm>
+#include <stack>
+
+using namespace std;
+
+int map[50][50], width, height;
+
+//これはコピペで覚えましょう。
+int dx[8] = { -1,0,1,-1,1,-1,0,1 }, dy[8] = { -1,-1,-1,0,0,1,1,1 }; 
+
+// 島問題ではrengecheckが必須。
+bool rangeCheck(int row, int col) {
+	if (row >= 0 && row <= height - 1 && col >= 0 && col <= width - 1)return true;
+	else {
+		return false;
+	}
+}
+
+// グラフの連結成分数は、深さ優先探索で計算
+
+void erase(int row, int col) {
+	map[row][col] = 0;
+
+	// 島が続く限り、eraseして0にする。
+	for (int i = 0; i < 8; i++) {
+		if (rangeCheck(row + dy[i], col + dx[i]) == true && map[row + dy[i]][col + dx[i]] == 1) {
+			erase(row + dy[i], col + dx[i]);
+		}
+	}
+}
+
+
+int main() {
+
+	int count;
+
+	while (true) {
+		scanf("%d %d", &width, &height);// こういう風にもにゅーりょくできるのか。
+		if (width == 0 && height == 0)break;// 最後の入力
+
+		for (int i = 0; i < height; i++) {
+			for (int k = 0; k < width; k++) {
+				scanf("%d", &map[i][k]);
+			}
+		}
+		//　ここまであっている。
+
+		count = 0;
+
+		// 変数名も参考になる。
+		for (int row = 0; row < height; row++) {
+			for (int col = 0; col < width; col++) {
+				if (map[row][col] == 1) {// 陸だったら
+					count++;// 島が一つ
+					erase(row, col);
+				}
+			}
+		}
+
+
+		printf("%d\n", count);
+
+	}
+
+
+	return 0;
+}
--- 深さ優先探索/深さ優先探索.cpp
+++ 深さ優先探索/深さ優先探索.cpp
@@ -1,55 +1,60 @@
-﻿#include <iostream>
-#include <vector>
+﻿#include <stdio.h>
+#include <iostream>
 #include <algorithm>
-
-#define inf ((1LL << 62) - (1LL << 31))
-#define all(x) (x).begin(), (x).end()
+#include <math.h>
+#include <stack>
 
 using namespace std;
-using ll = long long;
 
-// 深さ優先探索(DFS)の問題
+int common_time = 1, n;// どんな頭すればここで1,nを宣言できるのか。。
 
-// 整理
-// n = 数列Sの中の整数の数 :上限は100'000
-// S = 数列
-// q = 数列Tの数　:上限は50'000
-// C = SとTの共通する整数の個数
+int table[101][101], time_table[101][2];// time_tableを作成しているのが興味深い
 
-int main()
-{
-    int N = 0;
-    cin >> N;
-    vector<int> S(N);
-    for (int i = 0; i < N; i++)cin >> S[i];
+void func(int node) {// 一つの頂点について。
+	if (time_table[node][0] == 0) {// 発見時刻
+		time_table[node][0] = common_time++;
+	}
+	else {
+		return;//　すでに発見していたら再起関数の終わりにする。
+	}
+	for (int i = 1; i <= n; i++) {
+		if (table[node][i] == 1) {
+			func(i);
+		}
+	}
+	time_table[node][1] = common_time++;// 完了時刻を増やす。
+}
 
-    int M;
-    cin >> M;
-    vector<int> Q(M);
+int main() {
+	int row, num, col;
+	scanf("%d", &n);// 達人はscanfを使用している? intだからかな？
 
-    for (int i = 0; i < M; i++)cin >> Q[i];
-    sort(Q.begin(), Q.end());
+	for (int i = 1; i <= n; i++) {
+		for (int k = 1; k <= n; k++) {
+			table[i][k] = 0;// 0でtableを初期化。こうやって手間暇かけてやるのか・・・
+		}
+		time_table[i][0] = 0;// timetableも初期化しているのか。
+		time_table[i][1] = 0;
 
-    int left = 0, right, m, count = 0;
+	}
 
-    for (int i = 0; i < M; i++) {
-        right = N - 1;
-        m = (left + right) / 2;
+	for (int i = 1; i <= n; i++) {
+		scanf("%d %d", &row, &num);
+		for (int k = 1; k <= num; k++) {
+			scanf("%d", &col);
+			table[row][col] = 1;// 隣接する頂点の番号がcol rowは頂点の番号
+		}
+	}
 
-        while (left <= right) {
-            if (S[m] == Q[i]) {
-                count++;
-                left = m + 1;
-                break;
-            }
-            else if (S[m] > Q[i]) {
-                right = m - 1;
-            }
-            else { //S[m] < Q[i]
-                left = m + 1;
-            }
-            m = (left + right) / 2;
-        }
-    }
-    printf("%d\n", count);
+	for (int i = 1; i <= n; i++) {// ここで深さ優先探索DPS
+		func(i);
+	}
+
+	for (int i = 1; i <= n; i++) {
+		printf("%d %d %d\n", i, time_table[i][0], time_table[i][1]);
+		// 達人はprintfをする。
+		// 頂点の番号　頂点の発見時刻 完了時刻
+	}
+
+	return 0;
 }
